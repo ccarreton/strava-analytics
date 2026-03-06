8c997071093e5477efd7d07a5f1e14a0dc314bc6
ccarreton <cristianpeluko@gmail.com>
2026-03-06 20:01:32 +0100

    Update init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -6,12 +6,13 @@ cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS activities (
     id INTEGER PRIMARY KEY,
-    name TEXT,
-    type TEXT,
     start_date TEXT,
+    type TEXT,
+    name TEXT,
     distance REAL,
     moving_time INTEGER,
-    total_elevation_gain REAL
+    total_elevation_gain REAL,
+    raw_json TEXT
 )
 """)
 
