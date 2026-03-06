be53e0f308fd74e714500fad22f0cf081848bfc2
ccarreton <cristianpeluko@gmail.com>
2026-03-06 18:37:34 +0100

    Update weekly_report.py

--- scripts/weekly_report.py
+++ scripts/weekly_report.py
@@ -1,10 +1,20 @@
+import json
 import pandas as pd
 import requests
 
 BOT_TOKEN="8467992856:AAExZnsZCLS9S5y-KktePsQD2KACXRsJfC0"
 CHAT_ID="195248378"
 
-df=pd.read_json("data/strava_full_history.json")
+# abrir archivo
+with open("data/strava_full_history.json") as f:
+    text = f.read()
+
+# arreglar JSON concatenado
+text = text.replace("][", ",")
+
+data = json.loads(text)
+
+df = pd.DataFrame(data)
 
 df["date"]=pd.to_datetime(df["start_date"])
 df["week"]=df["date"].dt.to_period("W-MON").apply(lambda r: r.start_time)
@@ -17,7 +27,7 @@ week=df["week"].max()
 current=df[df["week"]==week]
 
 run=current[current["type"]=="Run"]["km"].sum()
-bike=current[current["type"].str.contains("Ride")]["km"].sum()
+bike=current[current["type"].str.contains("Ride",na=False)]["km"].sum()
 swim=current[current["type"]=="Swim"]["km"].sum()
 
 hours=current["hours"].sum()
