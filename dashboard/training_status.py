3493cf15630694ecfc17c6be45fe499fe974d1c5
ccarreton <cristianpeluko@gmail.com>
2026-03-08 18:21:57 +0100

    Create training_status.py

---
+++ dashboard/training_status.py
@@ -0,0 +1,34 @@
+import plotly.graph_objects as go
+
+
+def training_status_gauge(tsb):
+
+    fig = go.Figure(
+        go.Indicator(
+            mode="gauge+number",
+            value=tsb,
+            title={"text": "Training Status"},
+            gauge={
+                "axis": {"range": [-20, 20]},
+                "bar": {"color": "#2563EB"},
+                "steps": [
+                    {"range": [-20, -10], "color": "#FCA5A5"},
+                    {"range": [-10, -5], "color": "#FCD34D"},
+                    {"range": [-5, 5], "color": "#86EFAC"},
+                    {"range": [5, 10], "color": "#34D399"},
+                    {"range": [10, 20], "color": "#60A5FA"},
+                ],
+                "threshold": {
+                    "line": {"color": "black", "width": 4},
+                    "value": tsb
+                }
+            }
+        )
+    )
+
+    fig.update_layout(
+        height=250,
+        margin=dict(l=10, r=10, t=40, b=10)
+    )
+
+    return fig
